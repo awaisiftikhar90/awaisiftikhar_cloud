2b3a095fba1376757fc90dec5562c402bc8e7520
Awais Iftikhar <awaisiftikhar90@gmail.com>
2024-05-24 03:19:33 -0700

    Update Contact Submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,91 @@
+import { useState } from "react";
+
 export default function Contact() {
+    const [formData, setFormData] = useState({
+        subject: '',
+        message: '',
+        name: '',
+        email: ''
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const validateForm = () => {
+      let isValid = true;
+      const newErrors = {};
+
+      if (!formData.subject.trim()) {
+          newErrors.subject = 'Subject is required.';
+          isValid = false;
+      }
+
+      if (!formData.message.trim()) {
+          newErrors.message = 'Message is required.';
+          isValid = false;
+      }
+
+      if (!formData.name.trim()) {
+          newErrors.name = 'Name is required.';
+          isValid = false;
+      }
+
+      if (!formData.email.trim()) {
+          newErrors.email = 'Email is required.';
+          isValid = false;
+      } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+          newErrors.email = 'Email is invalid.';
+          isValid = false;
+      }
+
+      setErrors(newErrors);
+      return isValid;
+  };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        // Validate form
+        if (!validateForm()) {
+          return;
+        }
+
+        const apiUrl = 'https://n2vumam11h.execute-api.us-east-1.amazonaws.com/prod/contact';
+
+        try {
+          const response = await fetch(apiUrl, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(formData),
+          });
+
+          const responseData = await response.json();
+          if (response.ok) {
+                  alert('Email sent successfully!');
+                  setFormData({
+                      subject: '',
+                      message: '',
+                      name: '',
+                      email: ''
+                  });
+                  setErrors({}); 
+              } else {
+                  throw new Error(responseData.message);
+              }
+          } catch (error) {
+              alert('Failed to send email: ' + error.message);
+          }
+    };
+
+
     return (
         <section className="ftco-section contact-section ftco-no-pb" id="contact-section">
         <div className="container">
@@ -51,18 +138,22 @@ export default function Contact() {
 
           <div className="row no-gutters block-9">
             <div className="col-md-6 order-md-last d-flex">
-              <form action="#" className="bg-light p-4 p-md-5 contact-form">
+              <form onSubmit={handleSubmit} className="bg-light p-4 p-md-5 contact-form">
                 <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Your Name"/>
+                  <input type="text" className="form-control" placeholder="Your Name" name="name" value={formData.name} onChange={handleChange} />
+                  {errors.name && <div style={{ color: 'red' }}>{errors.name}</div>}
                 </div>
                 <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Your Email"/>
+                  <input type="email" className="form-control" placeholder="Your Email" name="email" value={formData.email} onChange={handleChange} />
+                  {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
                 </div>
                 <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Subject"/>
+                  <input type="text" className="form-control" placeholder="Subject" name="subject" value={formData.subject} onChange={handleChange} />
+                  {errors.subject && <div style={{ color: 'red' }}>{errors.subject}</div>}
                 </div>
                 <div className="form-group">
-                  <textarea name="" id="" cols="30" rows="7" className="form-control" placeholder="Message"></textarea>
+                  <textarea name="message" cols="30" rows="7" className="form-control" placeholder="Message" value={formData.message} onChange={handleChange} ></textarea>
+                  {errors.message && <div style={{ color: 'red' }}>{errors.message}</div>}
                 </div>
                 <div className="form-group">
                   <input type="submit" value="Send Message" className="btn btn-primary py-3 px-5"/>
